5faa5b109df0e0dcb6ec9d245826451609aa14dc
GUiAlvezzzzzz zzzzz <guilherme.santos11@eaportal.org>
2024-06-10 01:54:36 +0000

    style: estilização completa

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider} from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
 
-import { Home } from "./pages/Home.jsx"
+import { Home } from "./pages/Home"
 import { theme } from "./styles/theme.js"
 import { GlobalStyle } from "./styles/global.js"
 
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,6 @@
-const theme = {
+export const theme = {
     homeBackground: "#131518",
-    cardGradient: "radial-gradient(98% 96% 98.96% at 50% 0%, #3232A34 0% #181E27 100%)",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0% #181E27 100%)",
     textColor: "#969FAD",
     submitbuttonColor: "#FC7614",
     white: "#FFF",
